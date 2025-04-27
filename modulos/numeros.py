0ac9ada42200d3094ea2273029fd089022e4c66a
VitorMagioli <vitor.magioli10@gmail.com>
2025-04-27 22:03:29 +0000

    criação de pacotes no python para modularização

---
+++ modulos/numeros.py
@@ -0,0 +1,8 @@
+from uteis import numeros
+
+num = int(input('Digite um número: '))
+
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}.')
+print(f'O dobro de {num} é {numeros(num)}.')
+print(f'O triplo de {num} é {numeros(num)}.')
